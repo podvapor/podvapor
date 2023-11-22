8a250ccd058871cb98b1ed9feb93ed3bf4b3345f
Joe Sweeney <joe.sweeney224@gmail.com>
2023-11-21 17:49:31 -0700

    stuff

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -8,9 +8,9 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="d-flex align-items-center" style="gap: 2rem;">
-      <Button class="btn-danger" onClick={() => props.count.value -= 1}>-1</Button>
+      <Button class="btn-danger" onClick={() => props.count.value -= 1}>- 1</Button>
       <span class="h3 m-0">{props.count}</span>
-      <Button class="btn-success" onClick={() => props.count.value += 1}>+1</Button>
+      <Button class="btn-success" onClick={() => props.count.value += 1}>+ 1</Button>
     </div>
   );
 }
