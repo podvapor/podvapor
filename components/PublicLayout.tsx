cf13715cae6d2912a18f752785747b80b24105ae
Joe Sweeney <joe.sweeney224@gmail.com>
2023-11-27 14:33:31 -0700

    player that persists between pages

--- components/PublicLayout.tsx
+++ components/PublicLayout.tsx
@@ -1,7 +1,11 @@
 import { JSX } from "preact";
+import { Partial } from "$fresh/runtime.ts";
 
 export function PublicLayout(props) {
-  return <div class="container pt-3">
-    { props.children }
+  return <div class="container pt-3" f-client-nav>
+    <Partial name="public-page">
+      { props.children }
+    </Partial>
+    <audio controls src="https://podcast-media.sweeney.digital/episode_audio_files/72a6c29c-2414-4292-9289-5e6db8dda81b.mp3"></audio>
   </div>
 }
