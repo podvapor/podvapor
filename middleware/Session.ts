0753250e54547603f343cf9b16064e4f1218cba7
Joe Sweeney <joe.sweeney224@gmail.com>
2023-12-05 13:15:19 -0700

    upgrade session cookie security

--- middleware/Session.ts
+++ middleware/Session.ts
@@ -4,7 +4,11 @@ import { cookieSession, WithSession } from 'fresh-session'
 export type State = {} & WithSession
 
 const session = cookieSession({
-  maxAge: 900 // 15 minutes
+  maxAge: 900, // 15 minutes
+  path: '/',
+  httpOnly: true,
+  secure: true,
+  sameSite: 'Lax',
 })
 
 export function sessionHandler(req: Request, ctx: MiddlewareHandlerContext<State>) {
